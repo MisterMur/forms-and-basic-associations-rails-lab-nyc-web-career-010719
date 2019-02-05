8f5e02a21d326c6e6fb458358c12ff8ab6b15a7f
mistermur <brian.j.murillo@gmail.com>
2019-02-05 15:38:04 -0500

    Done.

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,14 @@
 class Note < ActiveRecord::Base
   # add associations here
+  belongs_to :song
+  #
+  # def note_contents
+  #     self.notes.map do|note|
+  #       note.content
+  #     end
+  # end
+  # 
+  # def note_contents=
+  #
+  # end
 end
